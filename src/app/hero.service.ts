c6a87245890af09ddebaf5dd103aa3f21a28cc65
unknown <wj.v.dun@gmail.com>
2019-04-03 09:26:42 +0200

    halfway there-commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Hero } from './hero';
+import { HEROES } from "./mock-heroes";
+import { Observable, of } from "rxjs";
+import { MessageService } from "./message.service";
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
+getHeroes (): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+}
+
+  getHero(id: number): Observable<Hero> {
+     // TODO: send the message _after_ fetching the hero
+     this.messageService.add(`HeroService: fetched hero id=${id}`);
+     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  
+
+  /** Log a HeroService message with the MessageService */
+private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
+
+}
